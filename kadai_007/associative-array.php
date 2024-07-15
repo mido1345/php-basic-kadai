23e824988672a2638faf97ba211f9d60227ec1bc
‘mido1345’ <mido.cla.v3v@gmail.com>
2024-07-15 13:46:28 +0900

    課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>課題07</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $onion_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($onion_data);
+    ?>
+  </p>
+</body>
+
+</html>
